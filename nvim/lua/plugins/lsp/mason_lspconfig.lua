3c530bff97c3f7e906c88668bd9686dc5e98d346
Andrew Biryukov <biryukov.andrej2000@mail.ru>
2025-07-22 22:09:43 +0300

    fix mason lspconfig settings

--- nvim/lua/plugins/lsp/mason_lspconfig.lua
+++ nvim/lua/plugins/lsp/mason_lspconfig.lua
@@ -16,11 +16,28 @@ return {
 			},
 		})
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.HINT] = "󰠠 ",
+					[vim.diagnostic.severity.INFO] = " ",
+				},
+				texthl = {
+					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+		})
 
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -33,7 +50,7 @@ return {
 		lspconfig.qmlls.setup({
 			filetypes = { "qml" },
 			capabilities = capabilities,
-			cmd = { "qmlls" },
+			cmd = { "qmlls", "-E" },
 		})
 
 		lspconfig.pyright.setup({
