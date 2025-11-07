5dc933bef19612e83fccebdbdf7dc8e3b0e9c16a
Andrew Biryukov <biryukov.andrej2000@mail.ru>
2025-11-07 12:38:05 +0300

    fix path

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias vim="nvim"
 
 alias DOTFILES="cd ~/.dotfiles/"
 
-alias vpn_connect='cat /etc/openvpn/moncloud/login.conf - | openvpn3 session-start --config ~/work/vpn/moncloud.ovpn'
-alias vpn_disconnect='openvpn3 session-manage --disconnect --config ~/work/vpn/moncloud.ovpn'
+alias vpn_connect='cat /etc/openvpn/moncloud/login.conf - | openvpn3 session-start --config ~/development/vpn/moncloud.ovpn'
+alias vpn_disconnect='openvpn3 session-manage --disconnect --config ~/development/vpn/moncloud.ovpn'
 alias vpn_list='openvpn3 sessions-list'
 alias vpn_status='openvpn3 session-stats --config moncloud'
